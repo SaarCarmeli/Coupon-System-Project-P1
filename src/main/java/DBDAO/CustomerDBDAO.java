c79f5da82db28cffd29848a8e8b7c9af85303409
Saar <carmeli.saar@gmail.com>
2022-02-18 13:48:34 +0200

    added DBdao, ObjectExtractionUtils (from database to objects), updated beans constructors (added Integer id parameter), updated junit tests accordingly

--- src/main/java/DBDAO/CustomerDBDAO.java
+++ src/main/java/DBDAO/CustomerDBDAO.java
@@ -24,7 +24,6 @@ public class CustomerDBDAO extends UserDAO<Integer, Customer> {
         }
     }
 
-
     @Override
     public Integer create(Customer customer) throws EntityCrudException {
         Connection connection = null;
