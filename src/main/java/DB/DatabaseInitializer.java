ceabd421eeebba0486af5169ff1082698a4ddfe4
SaarCarmeli <carmeli.saar@gmail.com>
2022-03-02 11:43:31 +0200

    Modified DatabaseInitializer

--- src/main/java/DB/DatabaseInitializer.java
+++ src/main/java/DB/DatabaseInitializer.java
@@ -12,22 +12,10 @@ import java.sql.SQLException;
 
 
 public class DatabaseInitializer {
-
-    private DatabaseInitializer() {
+    public DatabaseInitializer() {
     }
 
-    private static final Connection connection; //todo delete replace with runQuery
-
-    static {
-        try {
-            connection = ConnectionPool.getInstance().getConnection();
-        } catch (InterruptedException | SQLException e) {
-            throw new DBInitException();
-            //todo delete
-        }
-    }
-
-    public static void createTables() throws DBException, SQLException {
+    public static void createTables() {
         try {
             createSchema();
             createCategoriesTable();
@@ -35,10 +23,9 @@ public class DatabaseInitializer {
             createCustomersTable();
             createCouponsTable();
             createCustomerToCouponTable();
-        } catch (DBInitException e) {
+        } catch (DBInitException | DBException e) {
             e.getMessage();
         }
-        ConnectionPool.getInstance().returnConnection(connection);
     }
 
     private static void createSchema() throws DBInitException {
@@ -50,92 +37,42 @@ public class DatabaseInitializer {
     }
 
     public static void createCategoriesTable() throws DBException {
-        String sql = "CREATE TABLE IF NOT EXISTS `categories` (" +
-                "  `id` bigint NOT NULL AUTO_INCREMENT," +
-                "  `name` enum('FOOD','ELECTRICITY','RESTAURANT','VACATION') NOT NULL," +
-                "  PRIMARY KEY (`id`)" +
-                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.executeUpdate();
+            System.out.println("Created \"categories\" table: " + DBTools.runQuery(DBManager.CREATE_TABLE_CATEGORIES));
         } catch (SQLException e) {
-            throw new DBException("Failed to create categories table"); //todo how to integrate new method with existing custom exceptions?
+            throw new DBException("Failed to create \"categories\" table");
         }
     }
 
     public static void createCompaniesTable() throws DBException {
-        String sql = "CREATE TABLE IF NOT EXISTS `companies` (" +
-                "  `id` bigint NOT NULL AUTO_INCREMENT," +
-                "  `name` varchar(45) NOT NULL," +
-                "  `email` varchar(45) NOT NULL," +
-                "  `password` bigint NOT NULL," +
-                "  PRIMARY KEY (`id`)," +
-                "  UNIQUE KEY `name_UNIQUE` (`name`)," +
-                "  UNIQUE KEY `email_UNIQUE` (`email`)" +
-                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.executeUpdate();
+            System.out.println("Created \"companies\" table: " + DBTools.runQuery(DBManager.CREATE_TABLE_COMPANIES));
         } catch (SQLException e) {
-            throw new DBException("Failed to create companies table");
+            throw new DBException("Failed to create \"companies\" table");
         }
     }
 
     public static void createCustomersTable() throws DBException {
-        String sql = "CREATE TABLE IF NOT EXISTS `customers` (" +
-                "  `id` bigint NOT NULL AUTO_INCREMENT," +
-                "  `first_name` varchar(45) NOT NULL," +
-                "  `last_name` varchar(45) NOT NULL," +
-                "  `email` varchar(45) NOT NULL," +
-                "  `password` bigint NOT NULL," +
-                "  PRIMARY KEY (`id`)," +
-                "  UNIQUE KEY `email_UNIQUE` (`email`)" +
-                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.executeUpdate();
+            System.out.println("Created \"customers\" table: " + DBTools.runQuery(DBManager.CREATE_TABLE_CUSTOMERS));
         } catch (SQLException e) {
-            throw new DBException("Failed to create customers table");
+            throw new DBException("Failed to create \"customers\" table");
         }
     }
 
     public static void createCouponsTable() throws DBException {
-        String sql = "CREATE TABLE IF NOT EXISTS `coupons` (" +
-                "  `id` bigint NOT NULL AUTO_INCREMENT," +
-                "  `company_id` bigint NOT NULL," +
-                "  `category` varchar(45) NOT NULL," +
-                "  `title` varchar(45) NOT NULL," +
-                "  `description` varchar(45) DEFAULT NULL," +
-                "  `start_date` date NOT NULL," +
-                "  `end_date` date NOT NULL," +
-                "  `amount` int NOT NULL," +
-                "  `price` varchar(45) NOT NULL," +
-                "  `image` varchar(45) DEFAULT NULL," +
-                "  PRIMARY KEY (`id`)," +
-                "  CONSTRAINT `company_id` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`)" +
-                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.executeUpdate();
+            System.out.println("Created \"coupons\" table: " + DBTools.runQuery(DBManager.CREATE_TABLE_COUPONS));
         } catch (SQLException e) {
-            throw new DBException("Failed to create coupons table");
+            throw new DBException("Failed to create \"coupons\" table");
         }
     }
 
     public static void createCustomerToCouponTable() throws DBException {
-        String sql = "CREATE TABLE IF NOT EXISTS `customer_to_coupon` (" +
-                "  `customer_id` bigint NOT NULL," +
-                "  `coupon_id` bigint NOT NULL," +
-                "  KEY `customer.id_idx` (`customer_id`)," +
-                "  KEY `coupon.id_idx` (`coupon_id`)," +
-                "  CONSTRAINT `coupon.id` FOREIGN KEY (`coupon_id`) REFERENCES `coupons` (`id`)," +
-                "  CONSTRAINT `customer.id` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)" +
-                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.executeUpdate();
+            System.out.println("Created \"customer_to_coupon\" table: "+DBTools.runQuery(DBManager.CREATE_TABLE_CUSTOMER_TO_COUPON));
         } catch (SQLException e) {
-            throw new DBException("Failed to create customers and coupons table");
+            throw new DBException("Failed to create \"customer_to_coupon\" table");
         }
     }
 }
