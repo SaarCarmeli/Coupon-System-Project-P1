60fbd56d9d160a2f521b77b83f6aa2c015da626d
Saar <carmeli.saar@gmail.com>
2022-03-15 12:02:11 +0200

    Serialized Tests

--- src/test/java/Tests/LoginTests/AdminFacadeTest.java
+++ src/test/java/Tests/LoginTests/AdminFacadeTest.java
@@ -2,33 +2,54 @@ package Tests.LoginTests;
 
 import Beans.Company;
 import DB.DatabaseInitializer;
-import Exceptions.EntityAlreadyExistException;
-import Exceptions.EntityCrudException;
+import DB.Util.DBTools;
 import Facades.AdminFacade;
 import LoginManager.ClientType;
 import LoginManager.LoginManager;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
 
 public class AdminFacadeTest {
     public static AdminFacade adminFacade;
 
-    @BeforeClass
-    public static void initiation() {
+    @Before
+    public void initiation() {
         DatabaseInitializer.createTables();
         adminFacade = (AdminFacade) LoginManager.getInstance().login("admin@admin.com", "admin", ClientType.valueOf("ADMINISTRATOR"));
+    }
+
+    @After
+    public void finish() throws SQLException {
+        System.out.println("Dropped schema: " + DBTools.runQuery("DROP DATABASE `coupon_project`"));
     }
 
     @Test
     public void addCompanyTest() throws Exception {
         Company company = new Company("Itzik hooBanav", "itzB@itzmail.com", "19itzbanav_50");
-        try {
-            adminFacade.addCompany(company);
-            assertEquals(new Company(1, "Itzik hooBanav", "itzB@itzmail.com"), adminFacade.readCompany(1));
-        } catch (EntityAlreadyExistException | EntityCrudException e) {
-            throw new Exception(e.getMessage());
-        }
+        Company newCompany = new Company(1, "Itzik hooBanav", "itzB@itzmail.com");
+        adminFacade.addCompany(company);
+        assertEquals(newCompany.getId(), adminFacade.readCompany(1).getId());
+        assertEquals(newCompany.getName(), adminFacade.readCompany(1).getName());
+        assertEquals(newCompany.getEmail(), adminFacade.readCompany(1).getEmail());
+    }
+
+    @Test
+    public void updateCompanyTest() throws Exception {
+        Company company = new Company("Tzmigey Ferdinand von Schlauffer", "Tzmigimail@TzamigBusinessMail.com", "12345678");
+        Company newCompany;
+        adminFacade.addCompany(company);
+        company = adminFacade.readCompany(1);
+        company.setEmail("businessMail@TzamigMail.com");
+        company.setPassword("87654321");
+        adminFacade.updateCompany(company);
+        newCompany = adminFacade.readCompany(1);
+        assertEquals(company.getId(), newCompany.getId());
+        assertEquals(company.getName(), newCompany.getName());
+        assertEquals(company.getEmail(), newCompany.getEmail());
     }
 }
