9336a088c10d25139c12398ccc2af1790fba14ce
Saar <carmeli.saar@gmail.com>
2022-03-16 13:34:37 +0200

    added CompanyFacadeTest and CustomerFacadeTest

--- src/test/java/Tests/LoginTests/CustomerFacadeTest.java
+++ src/test/java/Tests/LoginTests/CustomerFacadeTest.java
@@ -1,4 +1,28 @@
 package Tests.LoginTests;
 
+import DB.DatabaseInitializer;
+import DB.Util.DBTools;
+import Facades.AdminFacade;
+import LoginManager.ClientType;
+import LoginManager.LoginManager;
+import org.junit.After;
+import org.junit.Before;
+
+import java.sql.SQLException;
+
 public class CustomerFacadeTest {
+    public static AdminFacade adminFacade;
+    public static int idCounter = 1;
+
+    @Before
+    public void initiation() {
+        DatabaseInitializer.createTables();
+        adminFacade = (AdminFacade) LoginManager.getInstance().login("admin@admin.com", "admin", ClientType.valueOf("ADMINISTRATOR"));
+        idCounter = 1;
+    }
+
+    @After
+    public void finish() throws SQLException {
+        System.out.println("Dropped schema: " + DBTools.runQuery("DROP DATABASE `coupon_project`"));
+    }
 }
