9af25e6195e5b4cc1897fe07633e9e7d67db5e1a
Saar <carmeli.saar@gmail.com>
2022-03-22 19:09:00 +0200

    finished javadoc for Util package

--- src/main/java/Util/DateUtil.java
+++ src/main/java/Util/DateUtil.java
@@ -5,8 +5,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtil {
+    /**
+     * Converts Date to String for TimerTask in CouponExpirationDailyJob
+     *
+     * @return Current Date in String
+     */
     public static String getCurrentDate() {
-        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy"); // todo where date comes from?
         return df.format(new Date());
     }
 }
